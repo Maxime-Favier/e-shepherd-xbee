fbade27414e9fc927b1bd275765667e79763d5c2
Maxime Favier <maxime1.favier@free.fr>
2018-01-20 17:25:09 +0100

    Add files via upload

---
+++ areasubmit.php
@@ -0,0 +1,23 @@
+<?php               //On récupère le pseudo et on le stocke dans une variable
+$bdd = new PDO('mysql:host=localhost;dbname=geomutton;charset=utf8', 'root', '');
+if(!isset($_POST['array1']))
+{
+	echo 'remplissez tous les champs';
+}
+else
+{
+	$reponse = $bdd->query('TRUNCATE TABLE area');
+	$id = htmlspecialchars($_POST['array1']); 
+	$tab = explode(',', $id);
+	
+	$collectionLength = count($tab);
+
+	for($i = 0; $i < $collectionLength; $i+=2)
+	{
+		$req = $bdd->prepare('INSERT INTO area (lat, longi) VALUES (:lat, :longi)');
+		$req->execute(array(
+		'lat' => $tab[$i],
+		'longi' => $tab[$i + 1],
+		));
+	}
+}
